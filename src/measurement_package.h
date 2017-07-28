dde6b61406e2ebcdf7c8419dddf4fa4c8cddaeab
Bryan Rathos <bryanrathos@gmail.com>
2017-07-28 18:42:07 +0530

    Changed editor settings to use indentations using spaces

--- src/measurement_package.h
+++ src/measurement_package.h
@@ -5,16 +5,16 @@
 
 class MeasurementPackage
 {
-	public:
-		long long timestamp_;
+  public:
+    long long timestamp_;
 
-		enum SensorType
-		{
-			LASER,
-			RADAR
-		}sensor_type_;
+    enum SensorType
+    {
+      LASER,
+      RADAR
+    }sensor_type_;
 
-	Eigen::VectorXd raw_measurements_;
+  Eigen::VectorXd raw_measurements_;
 };
 
 #endif /* MEASUREMENT_PACKAGE_H_ */
