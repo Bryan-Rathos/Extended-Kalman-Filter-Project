9293768926979a7d7ca35dffd57caec94f06ae19
Bryan Rathos <bryanrathos@gmail.com>
2017-07-29 23:27:50 +0530

    added UpdateEKF() fuction for radar measurement update

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "kalman_filter.h"
 
 using Eigen::MatrixXd;
@@ -25,7 +26,7 @@ void KalmanFilter::Predict()
   * predict the state
   */
   x_ = F_ * x_;
-  MatrixXd Ft = F_.transpose()
+  MatrixXd Ft = F_.transpose();
   P_ = F_ * P_ * Ft + Q_;
 }
 
@@ -38,8 +39,9 @@ void KalmanFilter::Update(const VectorXd &z)
   VectorXd z_pred = H_ * x_;
   VectorXd y = z - z_pred;
   MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd K = P_ * Ht * S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd K = PHt * S.inverse();
 
   // new estimate
   x_ = x_ + (K * y);
@@ -54,4 +56,31 @@ void KalmanFilter::UpdateEKF(const VectorXd &z)
   TODO:
   * update the state by using Extended Kalman Filter equations
   */
+  float rho = sqrt(x_(0) * x_(0) + x_(1) * x_(1));
+  float phi = atan2(x_(1), x_(0));
+  float rho_dot;
+
+  if (fabs(rho) < 0.0001)
+  {
+    rho_dot = 0;
+  } 
+  else
+  {
+    rho_dot = ((x_(0)*x_(2) + x_(1)*x_(3)))/rho;
+  }
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd S = H_ * PHt + R_;
+  MatrixXd K = PHt * S.inverse();
+
+  // new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
